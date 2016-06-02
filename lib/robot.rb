746600adce0060d20a8d7a4ec09c147f95571f0c
Tana Jukes <tana.jukes@gmail.com>
2016-06-02 11:25:49 -0700

    Spec 03 passes

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,10 +1,14 @@
 class Robot
-  attr_reader :position
+  CAPACITY = 250
+
+  attr_reader :position, :items
 
   def initialize
     @position = [0,0]
+    @items = []
   end
 
+# Movement methods
   def move_left
     @position[0] -= 1
   end
@@ -20,4 +24,16 @@ class Robot
   def move_down
     @position[1] -= 1
   end
+
+# Item interaction methods
+  def pick_up(item)
+    @items << item if items_weight + item.weight <= CAPACITY
+  end
+
+  def items_weight
+    weight = 0
+    @items.each { |item| weight += item.weight}
+    weight
+  end
+
 end
