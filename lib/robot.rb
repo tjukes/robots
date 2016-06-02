ac874c2c28e264a1dbb23ac6560855ab98947c54
Tana Jukes <tana.jukes@gmail.com>
2016-06-02 11:09:10 -0700

    Initial commit - starter files

---
+++ lib/robot.rb
@@ -0,0 +1,15 @@
+class Robot
+  attr_reader :position
+
+  def initialize
+    @position = [0,0]
+  end
+
+  def move_left
+    @position[0] -= 1
+  end
+
+  def move_right
+    @position[0] += 1
+  end
+end
